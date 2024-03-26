edb0e39c307335b8514973bff99acc87aca18c40
Chamu Mutezva <ckmutezva@gmail.com>
2024-03-26 05:00:31 +0200

    phonebook tutorial - mutations

--- phonebook-graphql/phonebook-frontend/src/App.jsx
+++ phonebook-graphql/phonebook-frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import PhoneForm from "./components/PhoneForm";
+import Notify from "./components/Notify";
+import { ALL_PERSONS } from "./helpers/queries";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const result = useQuery(ALL_PERSONS, {
+        refetchQueries: [{ query: ALL_PERSONS }],
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    if (result.loading) {
+        return <div>loading...</div>;
+    }
 
-export default App
+    const notify = (message) => {
+        setErrorMessage(message);
+        setTimeout(() => {
+            setErrorMessage(null);
+        }, 10000);
+    };
+
+    return (
+        <div>
+            <Notify errorMessage={errorMessage} />
+            <Persons persons={result.data.allPersons} />            
+            <PersonForm setError={notify} />
+            <PhoneForm  setError={notify} />
+        </div>
+    );
+};
+
+export default App;
--- phonebook-graphql/phonebook-frontend/src/main.jsx
+++ phonebook-graphql/phonebook-frontend/src/main.jsx
@@ -1,35 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
     uri: "http://localhost:4000",
     cache: new InMemoryCache(),
 });
 
-const query = gql`
-    query {
-        allPersons {
-            name
-            phone
-            street
-            city
-            id
-        }
-    }
-`;
-
-client.query({ query }).then((response) => {
-    console.log(response.data);
-});
-
-//ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>
+    </ApolloProvider>
 );
